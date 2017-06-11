5a98de08d2cfd9f76f2d3c9351e59dbe8bdd8196
agoalofalife <agoalofalife@gmail.com>
2017-06-11 16:28:48 +0300

    add get list process from commands 'ps'

--- process.go
+++ process.go
@@ -1,49 +1,43 @@
 package assistant
 
-//import "github.com/shirou/gopsutil"
 import (
 	"os/exec"
-	"log"
-	"os"
-	//process "github.com/mitchellh/go-ps"
 	"regexp"
-	"fmt"
 	"github.com/assistant/helpers"
+	"strconv"
+	"strings"
 )
 
 
-type Process interface {
+type Processing interface {
 	allPs() bool
+}
+type Process struct {
+	PID int
+	PPID int
+	TIME string
+	CMD string
+	TTY string
 }
 
 // list all process
-func allPs() bool {
-	//processes,_ := process.Processes()
-	//for _,ps := range processes{
-	//
-	//	log.Println(os.FindProcess(ps.Pid()))
-	//	os.Exit(2)
-	//}
+func allPs() []Process {
 	cmd ,_:= exec.Command("ps").Output()
+
 	regexTerminal := regexp.MustCompile("\\s[0-9]+\\s([A-z0-9]+)")
-
-	log.Println(helpers.GroupExclude(regexTerminal.FindAllStringSubmatch(string(cmd), -1)))
-	//regexTerminal.FindAllStringSubmatch(string(cmd), -1)
-	//log.Println(regexTerminal.SubexpNames())
-	os.Exit(2)
 	regexPid := regexp.MustCompile("\\s[0-9]+\\s")
+	regexTime := regexp.MustCompile("[0-9]{1,2}:[0-9]{1,2}\\.[0-9]{1,2}")
+	regexPath := regexp.MustCompile("[0-9]{1,2}:[0-9]{1,2}\\.[0-9]{1,2}\\s+(.+)")
+
+	listTerminal := helpers.GroupExclude(regexTerminal.FindAllStringSubmatch(string(cmd), -1))
 	listPid := regexPid.FindAllString(string(cmd), -1)
+	listTimes := regexTime.FindAllString(string(cmd), -1)
+	listPaths := helpers.GroupExclude(regexPath.FindAllStringSubmatch(string(cmd), -1))
 
-	fmt.Println( listPid )
-	//log.Println(string(cmd))
-	os.Exit(2)
-	for st := range cmd{
-		log.Println(string(st))
-
+	processList := make([]Process, len(listPid) + 1)
+	for i := 0; i <= len(listPid) -  1; i++{
+		pid ,_:= strconv.Atoi(strings.TrimSpace(listPid[i]))
+		processList[i] = Process{PID:pid, TIME:listTimes[i], CMD:listPaths[i],TTY:listTerminal[i]}
 	}
-
-	os.Exit(2)
-
-	return true
+	return processList
 }
-
