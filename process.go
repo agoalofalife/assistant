68477238cd6e1a058da3b23ed0f4a466dc76738e
agoalofalife <agoalofalife@gmail.com>
2017-06-15 17:51:26 +0300

    parse out console

--- process.go
+++ process.go
@@ -3,7 +3,7 @@ package assistant
 import (
 	"os/exec"
 	"regexp"
-	"github.com/assistant/helpers"
+	"github.com/agoalofalife/assistant/helpers"
 	"strconv"
 	"strings"
 	"encoding/json"
@@ -48,32 +48,30 @@ func New() *Process {
 }
 // list all process
 func (process *Process) allPs() []Process {
-	cmd ,_:= exec.Command("ps","-lf").Output()
-	mapProcesses := make(map[string]interface{})
+	cmd ,_:= exec.Command("ps","o", "uid,pid,ppid,f,c,stime,tty,rss,wchan,addr,time,stat,user,command").Output()
+	mapProcesses := make(map[int]map[string]interface{})
+	middleProcess := make(map[string]interface{})
 
 	// split all rows
 	rows := strings.Split(string(cmd), "\n")
+
 	//processes:= make([]Process, len(rows) -1 )
 	titleString := strings.Fields(rows[0])
 
-	// split title
-	//for _,nameFields := range strings.Fields(titleString) {
-	//	log.Println(nameFields)
-	//	for _,rowValue := range rows[1:]{
-	//		log.Println(rowValue)
-	//	}
-	//}
 
-	for _, rowValue := range rows[1:]{
-		log.Println(rows[1:])
-		os.Exit(2)
+	// the idea is that the string COMMAND can have gaps > 0 and
+	// and we leave it to the end
+	for indexRow, rowValue := range rows[1:]{
 		for index, attributeProcess := range strings.Fields(string(rowValue)) {
-
-			mapProcesses[titleString[index]] = attributeProcess
+			if index >= len(titleString) {
+				middleProcess[titleString[len(titleString) - 1]] = middleProcess[titleString[len(titleString) - 1]].(string) + " " + attributeProcess
+			} else{
+				middleProcess[titleString[index]] = attributeProcess
+			}
 		}
-		//processes[index] = Process{}
+		mapProcesses[indexRow] = middleProcess
 	}
-	log.Println(mapProcesses)
+	log.Println(mapProcesses[1])
 	os.Exit(2)
 	regexTerminal := regexp.MustCompile("\\s[0-9]+\\s([A-z0-9]+|.+)\\s[0-9]+:[0-9]+\\.[0-9]+")
 	regexPid := regexp.MustCompile("([0-9]+)\\s.+")
