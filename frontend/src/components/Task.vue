ee56838cef118ea94d665e0a856e6d80595844fe
agoalofalife <agoalofalife@gmail.com>
2017-06-21 21:26:55 +0300

    add modal edit task

--- frontend/src/components/Task.vue
+++ frontend/src/components/Task.vue
@@ -1,41 +1,66 @@
 <template>
+    <el-col :span="16" :offset="3" >
     <el-table :data="tasks" border style="width: 100%">
         <el-table-column
-                fixed
                 prop="id"
                 label="Id"
                 width="50">
         </el-table-column>
-        <!--<el-table-column-->
-                <!--prop="name"-->
-                <!--label="Name"-->
-                <!--width="170">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--prop="commandConsole"-->
-                <!--label="Command Console"-->
-                <!--width="170">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--prop="timeStart"-->
-                <!--label="Time Start"-->
-                <!--width="120">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--prop="timeOut"-->
-                <!--label="Time Out"-->
-                <!--width="120">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--fixed="right"-->
-                <!--label="Operations"-->
-                <!--width="120">-->
-            <!--<template scope="scope">-->
-                <!--<el-button @click="handleClick" type="text" size="small">Detail</el-button>-->
-                <!--<el-button type="text" size="small">Edit</el-button>-->
-            <!--</template>-->
-        <!--</el-table-column>-->
+        <el-table-column
+                prop="name"
+                label="Name"
+                width="170">
+        </el-table-column>
+        <el-table-column
+                prop="commandConsole"
+                label="Command Console"
+                width="200">
+        </el-table-column>
+        <el-table-column
+                prop="timeStart"
+                label="Time Start"
+                width="200">
+        </el-table-column>
+        <el-table-column
+                prop="timeOut"
+                label="Time Out"
+                width="200">
+        </el-table-column>
+        <el-table-column
+                label="Operations"
+                width="150">
+            <template scope="scope">
+                <el-button type="text" @click="openEdit" size="small">Edit</el-button>
+                <el-button  type="text" size="small">Detail</el-button>
+            </template>
+        </el-table-column>
     </el-table>
+
+        <el-dialog title="Edit" :visible.sync="modalEdit">
+            <el-form :model="form">
+                <el-form-item label="Name" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="Command console" :label-width="formLabelWidth">
+                    <el-input v-model="form.console" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="Command console" :label-width="formLabelWidth">
+                    <el-input v-model="form.console" auto-complete="off"></el-input>
+                </el-form-item>
+
+                <el-form-item label="Zones" :label-width="formLabelWidth">
+                    <el-select v-model="form.region" placeholder="Please select a zone">
+                        <el-option label="Zone No.1" value="shanghai"></el-option>
+                        <el-option label="Zone No.2" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+    <el-button @click="modalEdit = false">Cancel</el-button>
+    <el-button type="primary" @click="modalEdit = false">Confirm</el-button>
+    </span>
+    </el-dialog>
+    </el-col>
 </template>
 <style>
 
@@ -45,15 +70,30 @@
     export default{
         data(){
             return{
-                tasks:[]
+                tasks:[],
+                modalEdit:true,
+                form: {
+                    name: '',
+                    console:'',
+                    region: '',
+                    date1: '',
+                    date2: '',
+                    delivery: false,
+                    type: [],
+                    resource: '',
+                    desc: ''
+                },
+                formLabelWidth: '120px'
             }
+        },
+        methods : {
         },
         mounted(){
             this.$socket.emit('list:queues', function(data){
                 console.log('list task: ', JSON.parse(data))
 
                 this.tasks = JSON.parse(data)
-            })
+            }.bind(this))
         }
     }
 </script>
